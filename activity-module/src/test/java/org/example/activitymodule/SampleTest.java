c549f03b4158cbaa4367d01c9c87f5d1d14ea808
yfigueira <figueirapy@gmail.com>
2025-07-13 12:49:54 +0200

    activity service:: ci test

---
+++ activity-module/src/test/java/org/example/activitymodule/SampleTest.java
@@ -0,0 +1,13 @@
+package org.example.activitymodule;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}
