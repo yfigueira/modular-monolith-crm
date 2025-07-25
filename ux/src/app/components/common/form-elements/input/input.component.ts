f91d689611590edaa07726217c058d586a1c46d9
yfigueira <figueirapy@gmail.com>
2025-07-25 20:53:52 +0200

    ux:: created input form component

---
+++ ux/src/app/components/common/form-elements/input/input.component.ts
@@ -0,0 +1,23 @@
+import {Component, input, model} from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  imports: [],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css'
+})
+export class InputComponent {
+
+  id = input.required<string>();
+  label = input<string>('');
+  required = input<boolean>(false);
+  placeholder = input<string>('...');
+
+  value = model<string | undefined>('');
+
+  updateValue(currentValue: string) {
+    this.value.set(currentValue)
+  }
+
+  protected readonly HTMLInputElement = HTMLInputElement;
+}
